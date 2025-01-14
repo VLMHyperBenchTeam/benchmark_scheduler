f301ab640ed73579c2f829e5597d92fb7e0820df
Anton Shiryaev <med.phisiker@gmail.com>
2025-01-15 01:21:16 +1000

    feat(reader): add reader scrypt for reading nums from writer
    add reader scrypt for reading nums from writer

---
+++ pipeline/scripts/reader.py
@@ -0,0 +1,31 @@
+import os
+import time
+
+import torch
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+    # Имя файла, из которого будем читать данные
+    input_txt = "data/task1_output.txt"
+    output_txt = "data/task2_output.txt"
+
+    os.system("nvidia-smi")
+    print("torch.__version__", torch.__version__)
+    print("torch.cuda.is_available()", torch.cuda.is_available())
+    print("torch.cuda.current_device()", torch.cuda.current_device())
+    print("torch.cuda.device_count()", torch.cuda.current_device())
+    print("torch.cuda.get_device_name(0)", torch.cuda.get_device_name(0))
+
+    with open(input_txt, "r") as input_file, open(output_txt, "w") as output_file:
+        # Читаем файл построчно
+        for line in tqdm(input_file, desc="Processing lines", unit="line"):
+            number = int(line.strip())
+            doubled_number = number * 2
+
+            print(f"\nResult: {doubled_number}")
+            output_file.write(f"{doubled_number}\n")
+            output_file.flush()
+
+            # Ждем одну секунду
+            time.sleep(1)

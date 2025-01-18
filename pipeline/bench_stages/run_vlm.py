880b4e591396977e16ae969cbb3c83ebe39f8dd3
Anton Shiryaev <med.phisiker@gmail.com>
2025-01-18 23:32:42 +1000

    feat(bench stages): Add scripts for bench stages: run_vlm and run_eval
    Add scripts for bench stages: run_vlm.py and run_eval.py

--- pipeline/bench_stages/run_vlm.py
+++ pipeline/bench_stages/run_vlm.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+import torch
 from tqdm import tqdm
 
 
@@ -9,6 +10,11 @@ if __name__ == "__main__":
     filename = "data/task1_output.txt"
 
     os.system("nvidia-smi")
+    print("torch.__version__", torch.__version__)
+    print("torch.cuda.is_available()", torch.cuda.is_available())
+    print("torch.cuda.current_device()", torch.cuda.current_device())
+    print("torch.cuda.device_count()", torch.cuda.current_device())
+    print("torch.cuda.get_device_name(0)", torch.cuda.get_device_name(0))
 
     with open(filename, "w") as file:
         for i in tqdm(range(10), desc="Processing", unit="iteration"):
